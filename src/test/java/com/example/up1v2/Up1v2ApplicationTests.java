0118461d04aa08ce1f6a3a02ca11b194dc55274f
natikian <natikian@ya.ru>
2022-10-16 21:06:32 +0300

    add project 2

---
+++ src/test/java/com/example/up1v2/Up1v2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.up1v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Up1v2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
